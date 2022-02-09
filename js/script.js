ea5b61302e0f55af58525a308936a4e03465217b
Natalia <nat12121@mail.ru>
2022-02-09 16:02:28 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use sctrict";
+
+/*let number = 5;
+const leftBorderWidth = 1;
+
+alert('Hello');
+
+const result = confirm("Are you here?");
+console.log(result);*/
+
+/*const answer = +prompt('Вам есть 18?', "");
+console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt("Как Ваше имя?", "");
+answers[1] = prompt("Как Ваша фамилия?", "");
+answers[2] = prompt("Сколько Вам лет?", "");
+
+document.write(answers);*/
+
+/*---Интерполяция---
+const user = "Nata";
+
+alert(`Привет, ${user}`);*/
+
+/*--Операторы increment (++), decrement (--)
+let incr = 10,
+    decr = 10;
+    
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);*/
+
+/*---Операторы && || */
+const isCheked = true,
+      isClose = true;
+
+console.log(isCheked && isClose);
+
+const isCheked = true,
+      isClose = false;
+
+console.log(isCheked || isClose);
